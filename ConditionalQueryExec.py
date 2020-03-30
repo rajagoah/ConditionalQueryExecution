b5b0b3ef7c537d4e9cd877a813000c9b28f46409
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-03-29 20:32:37 -0400

    incoming project file

---
+++ ConditionalQueryExec.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("/Users/aakarsh.rajagopalan/Personal documents/Python projects/Conditional query execution/Test Table.csv")
+
+print(df[:0])
+
+print(df[0:2])
